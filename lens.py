5c7225084ca52792cd1e2544e2f78cb75e0e24e6
Sam Phelan <sep445@nyu.edu>
2018-05-30 15:21:52 -0400

    initial script created, possible lens found

--- lens.py
+++ lens.py
@@ -1,48 +1,63 @@
 import sys
 import numpy as np
 import csv
+import math
 
 #WIGGLE_ROOM=0.0042 # standard is 0.0041 but ill allow for a little bit extra
 max_distance = 15
 
 reuseCtr = 0;
-objID = []
-ra = []
-dec = []
-lensz = []
+
+galaxies = []
+targets = []
+
+# define class for astro-objects
+class astrObj():
+    def __init__(self, iD, ra, dec, z):
+        self.iD = iD
+        self.ra = ra
+        self.dec = dec
+        self.z = z
+
+def degreeToArcSec(ra1, ra2, dec1, dec2):
+    deltaRA = ra1 - ra2
+    dletaDec = dec1 - dec2
+
+    # adjust value of deltaRA for change in longitude
+    # see http://spiff.rit.edu/classes/phys445/lectures/astrom/astrom.html for explanation
+    avgDec = (dec1 + dec2) / 2
+    deltaRA = deltaRA * math.cos(avgDec)
+    # convert to arcsecs
+    deltaRA *= 3600
+    deltaDec *= 3600
+
+    #final result using quadratic formula
+    res = math.sqrt(math.pow(deltaRA, 2) + math.pow(deltaDec, 2))
+    return res
+
 
 with open('./Classes.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
-    for row in readCSV:
+    for row in readCSV:   
     	if (reuseCtr!=0):
-    		objID.append(row[1])
-    		ra.append(row[2])
-    		dec.append(row[3])
-    		lensz.append(row[4])
-    		
-            '''
-    		rdata = row[5].split(':')
-    		rconv = float(rdata[0])*15.+float(rdata[1])*0.25+float(rdata[2])*0.00416667
-    		ra.append(rconv)
-
-    		ddata = row[6].split(':')
-    		negative = 0
-    		if(ddata[0][0]=='-'):
-    			ddata[0] = -1.*float(ddata[0])
-    			negative = 1
-    		dconv = float(ddata[0]) + float(ddata[1])/60. + float(ddata[2])/3600
-    		if (negative==1):
-    			dconv = -1.*dconv
-    		dec.append(dconv)
-
-    		mags.append(row[7])
-    		magl.append(row[8])
-    		Nim.append(row[9])
-    		size.append(float(row[10])*WIGGLE_ROOM) #convert from arcseconds to degrees (with extra wiggle room)
-            '''
+            obj = astrObj(row[0], row[1], row[2], row[3])
+            if row[4] == 'GALAXY': #store all Galaxies in a separate array
+                galaxies.append(obj)
+            else:
+                targets.append(obj)
     	reuseCtr+=1
 
+    # go through each galaxy
+    for g in galaxies:
+        #search through all non-galaxies
+        potentials = []
+        for tar in targets:
+            dist = degreeToArcSec(g.ra, tar.ra, g.dec, tar.dec)
+            if dist <= max_distance and g.z == tar.z: #if in the neighborhood of 15 arcs or less
+                potentials.append(tar)
 
+        
+'''
 queries = open("Queries.txt","w") 
 
 queries.seek(0)
@@ -60,5 +75,5 @@ for i in num:
 	#queries.write("java -jar casjobs.jar execute -t 'mydb/2' 'drop table object_"+str(i)+"'\n\n")
 
 queries.close()
-
+'''
 print("donezo")
