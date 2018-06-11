30d046f25904a3f3fa6498f8687aadade2b7f998
Sam Phelan <sep445@nyu.edu>
2018-06-11 16:31:53 -0400

    fixed euclidean distance function

--- lens.py
+++ lens.py
@@ -3,22 +3,18 @@ import numpy as np
 import csv
 import math
 
-#WIGGLE_ROOM=0.0042 # standard is 0.0041 but ill allow for a little bit extra
-max_distance = 15
+max_distance = 15 #in arcsecs
 reuseCtr = 0
 galaxies = []
 targets = []
 
 file_name = ""
-only_galaxies = False
 
 if len(sys.argv) > 1: # can use any csv file, just make sure file name is included as FIRST argument (with file type)
     file_name = sys.argv[1]
 else:
     file_name = "Classes.csv"
 
-if len(sys.argv) > 2 and sys.argv[2] == "True":
-    only_galaxies = True
 
 # define class for astro-objects
 class astrObj():
@@ -29,20 +25,25 @@ class astrObj():
         self.z = float(z)
 
 def degreeToArcSec(ra1, ra2, dec1, dec2):
-    deltaRA = ra1 - ra2
-    deltaDec = dec1 - dec2
+   #convert all coordinates to radians
+   
+   ra1 = math.radians(ra1)
+   ra2 = math.radians(ra2)
+   dec1 = math.radians(dec1)
+   dec2 = math.radians(dec2)
 
-    # adjust value of deltaRA for change in longitude
-    # see http://spiff.rit.edu/classes/phys445/lectures/astrom/astrom.html for explanation
-    avgDec = (dec1 + dec2) / 2
-    deltaRA = deltaRA * math.cos(avgDec)
-    # convert to arcsecs
-    deltaRA *= 3600
-    deltaDec *= 3600
+   #calculate theta based on equation listed at https://physics.stackexchange.com/questions/224950/how-can-i-convert-right-ascension-and-declination-to-distances
+   #cos(theta) = sin(phi)sin(gamma) + cos(phi)cos(gamma)sin(beta-alpha)
+   
+   theta = math.acos(math.sin(dec1)*math.sin(dec2) + math.cos(dec1)*math.cos(dec2)*math.cos(ra1-ra2))
+   #theta = math.acos(cos) #calculates theta
 
-    #final result using quadratic formula
-    res = math.sqrt(math.pow(deltaRA, 2) + math.pow(deltaDec, 2))
-    return res
+   #convert back to degrees
+   theta = math.degrees(theta)
+   #return in arcsecs
+   theta *= 3600
+
+   return theta      
 
 
 with open('./'+file_name) as csvfile:
@@ -50,13 +51,10 @@ with open('./'+file_name) as csvfile:
     for row in readCSV:   
     	if (reuseCtr!=0):
             obj = astrObj(row[0], row[1], row[2], row[3])
-            if only_galaxies = False:
-                if row[4] == 'GALAXY': #store all Galaxies in a separate array
-                    galaxies.append(obj)
-                else:
-                    targets.append(obj) 
-            else:
+            if row[4] == 'GALAXY': #store all Galaxies in a separate array
                 galaxies.append(obj)
+            else:
+                targets.append(obj) 
     	reuseCtr+=1
 
 queries = open("Queries.txt","w") 
@@ -65,25 +63,22 @@ queries.seek(0)
 queries.truncate() # empty file before writing
 
 # go through each galaxy
-for g in galaxies:
-    #search through all non-galaxies
-    potentials = []
-    for tar in targets:
-        dist = degreeToArcSec(g.ra, tar.ra, g.dec, tar.dec)
-        print(dist)
-        if dist <= max_distance and g.z == tar.z: #if in the neighborhood of 15 arcs or less AND red shift is the same
-            potentials.append(tar)
+potentials = []
 
-counter = 1;
-for i in potentials:
-	
-	queries.write("casjobs run 'SELECT ALL p.objid,p.ra,p.dec,pz.z INTO mydb.object_"+str(counter)+" FROM SpecObj AS p JOIN Photoz AS pz ON pz.objid = p.objid where p.z=" + str(i.z)"'\n\n")
-	# and now to download table:
-	#queries.write("casjobs extract -b object_"+str(i)+" -F -type CSV -d ./Models/\n\n")
-	# and now to drop table remotely:
-	#queries.write("java -jar casjobs.jar execute -t 'mydb/2' 'drop table object_"+str(i)+"'\n\n")
-    counter++;
+# go through each galaxy
+#for g in galaxies:
+g = galaxies[0]
+#search through all non-galaxies
+for tar in targets:
+    dist = degreeToArcSec(g.ra, tar.ra, g.dec, tar.dec)
+    print(dist)
+    if dist <= max_distance and g.z == tar.z: #if in the neighborhood of 15 arcs or less
+        potentials.append(tar)
 
 queries.close()
 
 print("donezo")
+
+
+
+  
